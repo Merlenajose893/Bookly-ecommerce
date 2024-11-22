85bf5b9682f000cb99a84d7f1deeaf09a8d180d6
Merlena893 <merlenajose2003@gmail.com>
2024-11-22 16:04:28 +0530

    Changed

---
+++ config/db.js
@@ -0,0 +1,14 @@
+const mongoose=require('mongoose');
+const env=require('dotenv').config();
+const connectDB=async()=>{
+    try {
+        await mongoose.connect(process.env.MONGODB_URI);
+        console.log('Mongodb is connected');
+        
+    } catch (error) {
+        console.log('DB connected is error',error.message);
+        process.exit(1);
+        
+    }
+}
+module.exports=connectDB();
