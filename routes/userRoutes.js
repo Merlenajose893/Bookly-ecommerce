2d0fe40093229b149f2763e0464915e4cf1275e6
Merlena893 <merlenajose2003@gmail.com>
2025-03-14 09:46:07 +0530

    mmmmmmmmmmm

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -104,8 +104,8 @@ router.post('/getOrderDetails',profileController.getOrderDetails);
 router.get('/profileorder/viewOrder/:orderId',userController.viewOrder)
 router.post('/profileorder/cancel/:orderId',profileController.cancelOrder);
 router.get('/profileorder/cancel/:orderId',profileController.cancelOrder);
-// router.post('/profileorder/return/:orderId',profileController.returnOrder)
-router.post('/profileorder/return/:orderId', profileController.returnOrder);
+router.post('/profileorder/return/:orderId',profileController.returnOrder)
+// router.post('/profileorder/return/:orderId', profileController.returnOrder);
 
 router.get('/profileorder/invoice/:orderId',profileController.generateInvoice);
 // router.post('/profileorder/retry-payment/:orderId',profileController.retryPayment);
